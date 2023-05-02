3ec1e54acb3ad6fefc91c7149696a475864ea403
Abell Rede <abell.rede1@gmail.com>
2023-05-02 15:40:52 -0700

    Fix issue where the petpal image was not displaying on the webpage

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const app = express();
 const session = require('express-session');
 const ejsLayouts = require('express-ejs-layouts');
 const passport = require('passport');
+const path = require("path");
 const PORT = process.env.PORT || 3000;
-const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 app.set('view engine', 'ejs');
