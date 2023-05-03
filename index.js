a8f3279e2dcc6b3862b27e599f4898821182a83a
Abell Rede <abell.rede1@gmail.com>
2023-05-03 10:37:01 -0700

    Change file structure and convert all html files to ejs files

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 const session = require('express-session');
 const ejsLayouts = require('express-ejs-layouts');
 const passport = require('passport');
+const petpalController = require('./controller/petpal-controller');
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
-app.set('view engine', 'ejs');
 app.use(session({
     secret: "secret",
     resave: false,
@@ -23,6 +23,13 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.set('view engine', 'ejs');
+
+// Routes start here
+app.get('/', petpalController.index);
+app.get('/login', petpalController.login);
+app.get('/signup', petpalController.signup);
 
 app.listen(PORT, () => {
     console.log('Server is running on port 3000');
