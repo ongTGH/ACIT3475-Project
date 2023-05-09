8d60e6d94c9563fe45dd401e461286fd1bd1adc4
a-rede <abell.rede1@outlook.com>
2023-05-08 18:59:41 -0700

    Remove pet-model.js and remove pet search function from user model

--- database/user-model.js
+++ database/user-model.js
@@ -4,10 +4,6 @@ const userErrCheck = (user) => {
     if (!user) throw new Error('User not found');
 };
 
-const petErrCheck = (pet) => {
-    if (!pet) throw new Error('Pet not found');
-};
-
 const userModel = {
     getAllUsers: () => {
         return database;
@@ -31,14 +27,6 @@ const userModel = {
         const user = database.find((user) => user.lastName.toLowerCase() === lastName.toLowerCase());
         userErrCheck(user);
         return user;
-    },
-    getUserPet: (id, petName) => {
-        const user = database.find((user) => user.id === id);
-        userErrCheck(user);
-        const pet = user.pets.find((pet) => pet.petName.toLowerCase() === petName.toLowerCase());
-        petErrCheck(pet);
-        return pet;
-
     },
     searchUser: (search) => {
         return database.filter(
